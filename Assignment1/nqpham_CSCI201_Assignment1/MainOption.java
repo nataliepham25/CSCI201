00761837b3203bcc53c5d76223cebf27e71ff1f9
Natalie Pham <nataliepham525@gmail.com>
2021-02-09 17:45:52 -0800

    Create MainOption.java

---
+++ Assignment1/nqpham_CSCI201_Assignment1/MainOption.java
@@ -0,0 +1,21 @@
+package nqpham_CSCI201_Assignment1;
+
+import java.util.Scanner;
+
+/*
+ * Natalie Pham
+ * CSCI 201
+ * Professor Papa
+ * Runs the menu
+ */ 
+
+public class MainOption {
+	
+	static Scanner sc1= new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		Menu menu = new Menu(sc1);
+		
+	}
+}
